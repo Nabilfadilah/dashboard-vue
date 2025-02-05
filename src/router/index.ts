97e1605e27a052abca261bc1dcd4696ef9d56f68
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-05 10:01:07 +0700

    Memberikan path children

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileView from '@/views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,14 +9,18 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      children: [
+        {
+          path: '/profile',
+          name: 'profile',
+          component: ProfileView,
+        },
+        {
+          path: '/about',
+          name: 'about',
+          component: () => import('../views/AboutView.vue'),
+        },
+      ]
     },
   ],
 })
