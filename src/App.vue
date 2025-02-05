d4cac3f282fa6ac249817c801e3203485d6b9616
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-05 10:00:42 +0700

    Menambah router view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div>
+    <router-view />
+  </div>
 </template>
 
 <style scoped>
+
+
 </style>
